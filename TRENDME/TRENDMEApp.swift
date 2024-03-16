af028448f584d2a43a0e7ad993a1e8292de632ab
Waruna Madushanka Wimalaweera <warunamadushanka456@gmail.com>
2024-03-16 17:48:15 +0530

    Initial Commit

---
+++ TRENDME/TRENDMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRENDMEApp.swift
+//  TRENDME
+//
+//  Created by Mangi Wimalaweera on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct TRENDMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
